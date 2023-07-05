f8fe9f709662d70f47bb37e20a45a89f1c6eb5b4
Shivam <shivam.bisht@thoughts2binary.com>
2023-07-05 19:17:45 +0530

    Writing basic test for Dappcord contract

--- test/Dappcord.js
+++ test/Dappcord.js
@@ -1,9 +1,45 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
 describe("Dappcord", function () {
+  let deployer, user;
 
-})
+  let dappcord;
+
+  const NAME = "Dappcord";
+  const SYMBOL = "DC";
+
+  beforeEach(async () => {
+    // setup accounts
+    [deployer, user] = await ethers.getSigners();
+
+    const Dappcord = await ethers.getContractFactory("Dappcord");
+    dappcord = await Dappcord.deploy(NAME, SYMBOL);
+  });
+
+  describe("Deployment", function () {
+    it("Sets the name", async () => {
+      // Fetch name
+      let result = await dappcord.name();
+      // check name
+      expect(result).to.equal(NAME);
+    });
+
+    it("Sets the symbol", async () => {
+      // Fetch Symbol
+      let result = await dappcord.symbol();
+      // Check Symbol
+      expect(result).to.equal(SYMBOL);
+    });
+
+    it("Sets the owner", async () => {
+      // Fetch Owner
+      let result = await dappcord.owner();
+      // Check Owner
+      expect(result).to.equal(deployer.address);
+    });
+  });
+});
